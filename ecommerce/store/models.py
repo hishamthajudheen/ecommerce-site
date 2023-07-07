6fa0fb7587dadac7822fa5a6328f75e20ca4f8fd
Hisham Thajudheen <hishamthajudheen@gmail.com>
2023-07-07 23:30:50 +0530

    Setup database models

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -4,5 +4,22 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
-    slug = models.SlugField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=255, unique=True)    
+    class Meta:
+        verbose_name_plural = 'categories'
     
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255, default='unbranded')
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)   
+    image = models.ImageField(upload_to='images/')     
+    class Meta:
+        verbose_name_plural = 'products'
+    
+    def __str__(self):
+        return self.title

3fed53cb65cb06be55947f7500bcb3b5be847266
Hisham Thajudheen <hishamthajudheen@gmail.com>
2023-07-08 17:44:05 +0530

    Updated the storefront to display all the products in database with images

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store',
+    'store', #installed app
 ]
 
 MIDDLEWARE = [
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'store.views.categories', #updated
             ],
         },
     },
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Product, Category
 
 # Create your views here.
 
 def store(request):
-    return render(request, 'store/store.html')
+    all_products = Product.objects.all()
+    context = {'all_products':all_products}
+    return render(request, 'store/store.html', context=context)
+
+def categories(request):
+    all_categories = Category.objects.all()
+    return {'all_categories':all_categories}

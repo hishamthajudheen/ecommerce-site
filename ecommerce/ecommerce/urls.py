d67da51266040597a221e904e4c1b3e19b07a2bc
Hisham Thajudheen <hishamthajudheen@gmail.com>
2023-07-08 16:38:56 +0530

    Set up the store front page with bootstrap styling

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     path('admin/', admin.site.urls),
+    path('', include('store.urls')),
     
 ]
 

faf8da89751e40312b8ffe52d0119086632d7601
Hisham Thajudheen <hishamthajudheen@gmail.com>
2023-07-09 21:25:37 +0530

    working on cart add functionality

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -10,22 +10,17 @@ def cart_summary(request):
    return render(request, 'cart/cart-summary.html')
 
 def cart_add(request):
-   cart = Cart(request)
-   
-   if request.POST.get('action') == 'post':
-      product_id = int(request.POST.get('product_id'))
-      product_quantity = int(request.POST.get('product_quantity'))
-      
-      product = get_object_or_404(Product, id=product_id)
-      
-      cart.add(product=product, product_qty=product_quantity)  
-      
-      response = JsonResponse({
-            'the product is':product.title,
-            'and the product quantity':product_quantity
-         })
-      
-      return response
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_quantity = int(request.POST.get('product_quantity'))
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.add(product=product, product_qty=product_quantity)
+        response = JsonResponse({'qty': cart_quantity})
+
+        return response
 
 def cart_delete(request):
    pass
